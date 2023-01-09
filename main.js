c1e8a6339de1aacef013551528d3ecab0eb9c1c0
Mufaddalkapoor <105203700+Mufaddalkapoor@users.noreply.github.com>
2023-01-09 14:39:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+  Webcam.set({
+    width:350,
+    height:300,
+    image_format: 'png',
+    png_quality:90
+});
+
+  
+
+  function take_snapshot() {
+
+    webcam.snap(function(data_uri)
+    {
+
+        document.getElementById("result").innerHTML = '<img id="capture_image" src="'+data_uri+'"/>';
+        
+    });
+
+  }
+
+  console.log('ml5 version:',ml5.version);
+  classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/K6IeR9-J7/modle.json',modelLoaded);
+
+function modelLoaded()
+{
+     console.log('Model Loaded!!!');
+}
+
+function check()
+{
+  img=document.getElementById('captured_image');
+  classifier.classify(img,gotResult)
+}
+
+function gotResult(error,results)
+{
+  if(error){
+    console.error(error);
+  }
+  else{
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+  }
+}
